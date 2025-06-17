d7bd9156572fb843dac8492a7b2d2c3085806f51
Gab2601 <gabrielcarlier26@icloud.com>
2025-06-17 21:26:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,25 @@
-const CACHE_NAME = "hangar-avion-cache-v1";
-const urlsToCache = ["/", "/index.html", "/styles.css", "/login.html"];
+const CACHE_NAME = "hangar-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "icon.png",
+  "icon.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
